5a2b5623d8747ac0fb8b45c45ddf92a72ffcb781
Melisa Encinas <112040379+MelEncinas@users.noreply.github.com>
2022-10-05 18:25:29 -0300

    Se agregaron nuevas acciones con javascript

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
-const boton = document.querySelector ('#switch');
+/*MODO NOCUTRNO*/
+const boton = document.querySelector('#switch');
 boton.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     boton.classList.toggle('active');
 })
 
+/*SONIDO DE BOTÓN MODO NOCTURNO*/
 var sound = new Audio();
-sound.src= "./sonidos/audio1.mp3";
+sound.src = "./sonidos/audio1.mp3";
+
+
+
+/*FORMULARIO DE REGISTRO*/
+var nombre = document.getElementById("nombre")
+var apellido = document.getElementById("apellido")
+var email = document.getElementById("email")
+var usuario = document.getElementById("usuario")
+var contrase = document.getElementById("contraseña")
+var cel = document.getElementById("cel")
+var form = document.getElementById("form")
+var parrafo = document.getElementById("warnings")
+
+function enviarFormulario() {
+    console.log("Enviando formulario...")
+    console.log(nombre.value)
+    console.log(apellido.value)
+    console.log(email.value)
+    console.log(usuario.value)
+    console.log(contrase.value)
+    console.log(cel.value)
+
+}
+
+/*El resto de JS está en la página index.html y en cursos.html*/
