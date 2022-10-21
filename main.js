857624309af5b5d245ff22869706b38cb1af741c
Abril Ivon Barros <abril.ivon.barros@gmail.com>
2022-10-21 00:15:56 -0300

    Se organizó el main.js y se agregó objeto constructor en areapersonal.html

--- main.js
+++ main.js
@@ -9,6 +9,19 @@ boton.addEventListener('click', () => {
 var sound = new Audio();
 sound.src = "./sonidos/audio1.mp3";
 
+/* Efecto Maquina de Escribir */
+let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+});
+
+typewriter
+    .typeString('CURSOS DE APOYO GRATUITOS PARA ESTUDIANTES')
+    .pauseFor(200)
+    .deleteChars(10)
+    .start();
 
 
 /*FORMULARIO DE REGISTRO*/
@@ -32,18 +45,8 @@ function enviarFormulario() {
 
 }
 
-/*El resto de JS está en la página index.html y en cursos.html*/
-
-/* Efecto Maquina de Escribir */
-let app = document.getElementById('typewriter');
-
-let typewriter = new Typewriter(app, {
-loop: true,
-delay: 75,
-});
-
-typewriter
-.typeString('CURSOS DE APOYO GRATUITOS PARA ESTUDIANTES')
-.pauseFor(200)
-.deleteChars(10)
-.start();
+//El resto de JS está en la página index.html y en cursos.html
+//El desafío de Storage y Json se encuentran en la carpeta con el mismo nombre
+//La consigna de la pre-entrega n°3 se encuentra en la página areapersonal.html, 
+//donde simulamos el ingreso de usuarios en el día; y en la página cursos.html
+//donde agregamos un array
